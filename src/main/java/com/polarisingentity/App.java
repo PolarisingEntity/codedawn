dc99d351cbaaf7d39c6ce78ec108cb9045908f2e
PolarisingEntity <fvbst@outlook.com>
2021-12-23 15:14:07 +0100

    Created barebones structure

---
+++ src/main/java/com/polarisingentity/App.java
@@ -0,0 +1,10 @@
+package com.polarisingentity;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // TODO menu implementeren        
+    }
+}
